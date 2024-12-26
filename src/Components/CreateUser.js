0300f3a6338a02fc8344b28c227ac69af1565201
TuanTu1007 <21522747@gm.uit.edu.vn>
2024-12-27 04:26:59 +0700

    update

--- src/Components/CreateUser.js
+++ src/Components/CreateUser.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase= createClient(
-    "https://ysuzgtfwzcmdxfgefkfc.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzdXpndGZ3emNtZHhmZ2Vma2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2MzM1NDEsImV4cCI6MjA1MDIwOTU0MX0.xlqht4tneP65Xkv2uWyUFrfmkN5sZhfGspY_GqKUqdk"
+    "LINK",
+    "LINK"
 )
